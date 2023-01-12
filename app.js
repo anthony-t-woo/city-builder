9d4eafde6806f3948969596a224d3431479112c6
ANTHONY WOO <anthony.woo14@gmail.com>
2023-01-12 16:15:37 -0700

    forgot to add report div

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 /* Imports */
 
 /* Get DOM Elements */
+const climateDropdown = document.getElementById('climate-dropdown');
+const parkDropdown = document.getElementById('park-dropdown');
+const architectureDropdown = document.getElementById('architecture-dropdown');
 
 /* State */
 
